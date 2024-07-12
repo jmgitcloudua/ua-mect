2132c7c4a5728b799395c292fb738acfeb8f58e8
Jodionisio Muachifi <jodionisiomuachifi@ua.pt>
2024-07-12 18:45:42 +0100

    3ano-leci

---
+++ 3ano-LECI/Iºsemestre/PECI/List of project proposals-20211016/readMe.txt
@@ -0,0 +1,7 @@
+Lista de projetos interessantes
+Projeto 3**
+Projeto 5*
+Projeto 6*
+Projeto 8*
+Projeto 9*
+Projeto 11**
---
+++ 3ano-LECI/Iºsemestre/PECI/project/sockets_test/client.py
@@ -0,0 +1,44 @@
+import signal
+import socket
+import sys
+import psutil
+import time
+from colorama import Fore
+
+intro = Fore.GREEN + '''
+        
+        #############################################################
+        #   Projeto em Engenharia de Computadores e Informática     #           
+        #       Jodionísio Muachifi - 97147                         #
+        #       LECI 2021-2022                                      #
+        #############################################################
+'''
+print(intro + "\n")
+
+def signal_handler(sg, fr):
+    print("\nDone Successfully!")
+    sys.exit(0)
+
+print(Fore.CYAN +" ######## CLIENT START #########")
+signal.signal(signal.SIGINT, signal_handler)
+print("Press Ctrl+C to exit ...")
+
+
+
+ip = "127.0.0.1"
+port = 8008
+
+socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketTCP.connect((ip, port))
+
+while True:
+    try:
+        msg = ("CPU: "+str(psutil.cpu_percent())+" %   Memory: "+str(psutil.virtual_memory()[2])+"%").encode()
+        if len(msg)>0:
+            socketTCP.send(msg)
+            response = socketTCP.recv(4096).decode()
+            print("Server response: {}".format(response))
+        time.sleep(2)
+    except (socket.timeout, socket.error):
+        print("Server error. Out!")
+        sys.exit(0)
